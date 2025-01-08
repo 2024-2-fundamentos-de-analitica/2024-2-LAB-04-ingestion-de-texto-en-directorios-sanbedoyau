d02d80da4cf8b489d121e74abee353f569612625
sanbedoyau <sanbedoya@unal.edu.co>
2025-01-08 01:25:28 -0500

    Lab done

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,18 +1,18 @@
 # pylint: disable=import-outside-toplevel
 # pylint: disable=line-too-long
 # flake8: noqa
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
 
+from homework.utils import load_file, create_ouptput_directory
+from random import shuffle
+import pandas as pd
 
 def pregunta_01():
-    """
+    '''
     La información requerida para este laboratio esta almacenada en el
-    archivo "files/input.zip" ubicado en la carpeta raíz.
+    archivo 'files/input.zip' ubicado en la carpeta raíz.
     Descomprima este archivo.
 
-    Como resultado se creara la carpeta "input" en la raiz del
+    Como resultado se creara la carpeta 'input' en la raiz del
     repositorio, la cual contiene la siguiente estructura de archivos:
 
 
@@ -46,16 +46,16 @@ def pregunta_01():
     ```
 
     A partir de esta informacion escriba el código que permita generar
-    dos archivos llamados "train_dataset.csv" y "test_dataset.csv". Estos
-    archivos deben estar ubicados en la carpeta "output" ubicada en la raiz
+    dos archivos llamados 'train_dataset.csv' y 'test_dataset.csv'. Estos
+    archivos deben estar ubicados en la carpeta 'output' ubicada en la raiz
     del repositorio.
 
     Estos archivos deben tener la siguiente estructura:
 
     * phrase: Texto de la frase. hay una frase por cada archivo de texto.
-    * sentiment: Sentimiento de la frase. Puede ser "positive", "negative"
-      o "neutral". Este corresponde al nombre del directorio donde se
-      encuentra ubicado el archivo.
+    * target: Sentimiento de la frase. Puede ser 'positive', 'negative'
+        o 'neutral'. Este corresponde al nombre del directorio donde se
+        encuentra ubicado el archivo.
 
     Cada archivo tendria una estructura similar a la siguiente:
 
@@ -70,4 +70,23 @@ def pregunta_01():
     ```
 
 
-    """
+    '''
+    emotions = ['positive', 'negative', 'neutral']              # Lista que contiene las emociones
+    in_path = 'files/input'                                     # Path del directorio de inputs
+    out_path = 'files/output'                                   # Path del directorio de outputs
+    test = []                                                   # Lista que guardará los datos del directorio test
+    train = []                                                  # Lista que guardará los datos del directorio train
+    for emotion in emotions:                                    # Se recorre cada emoción
+        test += load_file(f'{in_path}/test/{emotion}')          # A cada lista se le añaden los datos extraídos de cada archivo
+        train += load_file(f'{in_path}/train/{emotion}')        # de ambos directorios con el formato (emoción, frase)
+    shuffle(test)   # Randomizar cada lista, totalmente
+    shuffle(train)  # inncesario, pero coincide con el ejemplo
+    test_data = {'phrase': [phrase for _, phrase in test],      # Diccionarios con los datos de cada directorio
+                 'target': [emotion for emotion, _ in test]}    # para la creación de cada Dataframe
+    train_data = {'phrase': [phrase for _, phrase in train],
+                  'target': [emotion for emotion, _ in train]}
+    test_dataset = pd.DataFrame(test_data)                      # Creación de cada DataFrame
+    train_dataset = pd.DataFrame(train_data)
+    create_ouptput_directory(out_path)                          # Función para crear y limpiar el directorio de outputs
+    test_dataset.to_csv(f'{out_path}/test_dataset.csv')         # Creación de los csv a partir de cada DataFrame
+    train_dataset.to_csv(f'{out_path}/train_dataset.csv')
